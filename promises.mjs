7aa69357a5bbeaa97f93f039e60fdca1a93bfc8b
defycab <love.bellinder@gmail.com>
2024-09-25 23:30:13 +0200

    error handling when fetching data

--- promises.mjs
+++ promises.mjs
@@ -4,3 +4,21 @@ fetch("https://randomuser.me/api/?results=10")
   .then((response) => response.json())
   .then((data) => data.results)
   .then((results) => console.log(`We got ${results.length} rows`))
+
+fetch("https://randomuser.me/api/?results=1")
+  .then((response) => response.json())
+  .then((data) => data.results)
+  .then((results) =>
+    console.log(
+      `We got the name of ${results[0].name.title} ${results[0].name.first} ${results[0].name.last}`
+    )
+  )
+
+import { readFile } from "fs"
+readFile("thisFileDoExist.json", "utf8", (err, file) => {
+  if (err) {
+    console.log("We got an error", { err })
+  }
+  // no error
+  console.log(`That file has ${file.split("\n").length} lines`)
+})
